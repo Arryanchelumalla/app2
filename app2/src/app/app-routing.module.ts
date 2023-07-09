6db68fd168bfa748ce78c7c956bb767ce2b90cbe
arryanchelumalla <rahul.chelumalla@gmail.com>
2023-07-09 19:43:29 +0530

    all component show in one page

--- app2/src/app/app-routing.module.ts
+++ app2/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProdComponent } from './prod/prod.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:ProdComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
